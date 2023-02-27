9b54075bbdba78d564021499ac0ef94217c78801
Aayush Gupta <aayush.gupta555@gmail.com>
2023-02-27 22:45:55 +0530

    Snake head movement
    Added motion to the snake and control it with arrow keys.

--- Snake Game/Assets/Scripts/PlayerController.cs
+++ Snake Game/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Speed;
+    private Vector2Int direction = Vector2Int.up;
+    private Vector3 SpriteRotate = new Vector3(0f, 0f, 0f);
+    private float horizontal;
+    private float vertical;
+
+    private void Awake()
     {
-        
+        transform.position = new Vector3(direction.x, direction.y, 0f);
+        transform.rotation = new Quaternion(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z, 0f);
+    }
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        Movement(horizontal, vertical);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Movement(float Horizontal, float Vertical)
     {
-        
+
+        if (Horizontal > 0)
+        {
+            direction = Vector2Int.right;
+            transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z - 90f);
+        }
+        else if (Horizontal < 0)
+        {
+            direction = Vector2Int.left;
+            transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z + 90);
+        }
+
+        if (Vertical > 0)
+        {
+            direction = Vector2Int.up;
+            transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z);
+        }
+        else if(Vertical < 0)
+        {
+            direction = Vector2Int.down;
+            transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z + 180);
+        }
+        Vector2 MoveDistance = new Vector2(direction.x *Time.deltaTime * Speed, direction.y * Time.deltaTime * Speed);
+        transform.position = new Vector3(transform.position.x + MoveDistance.x, transform.position.y + MoveDistance.y, 0f);
+
+
     }
 }
