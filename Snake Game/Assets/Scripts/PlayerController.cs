6394fcfa9971b9411dbb390088841716089770bd
Aayush Gupta <aayush.gupta555@gmail.com>
2023-03-04 01:12:36 +0530

    Snake Growth
    Implemented snake growing feature on eating food. Changed the movement mechanism and scene setup to make grid  dependent movement

--- Snake Game/Assets/Scripts/PlayerController.cs
+++ Snake Game/Assets/Scripts/PlayerController.cs
@@ -4,69 +4,118 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed;
+    public float TimetoMove = 0.2f;
     public GameObject Segment;
-
-    private Vector2Int direction = Vector2Int.up;
+    [SerializeField]
+    public List<GameObject> segments;
+    
+    private bool IsMoving = false;
+    private Vector3 orignalPos, TargetPos;
+    private Vector3 direction = Vector2.up;
     private Vector3 SpriteRotate = new Vector3(0f, 0f, 0f);
+    private Vector3 BodyOffset;
     private float horizontal;
     private float vertical;
-    private List<GameObject> segments;
 
     private void Awake()
     {
         transform.position = new Vector3(direction.x, direction.y, 0f);
-        transform.rotation = new Quaternion(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z, 0f);
+        transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z);
     }
 
-  
+    private void Start()
+    {
+        transform.position = new Vector3(0f,1f, 0f);
+        segments = new List<GameObject>();
+        segments.Add(gameObject);
+        segments.Add(Segment);
+    }
+
     private void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-        Movement(horizontal, vertical);
+
+        if (!IsMoving)
+        {
+            float x = 0.05f;
+            if (horizontal > 0 && direction != Vector3.left)
+            {
+                direction = Vector3.right;
+                transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z - 90f);
+                BodyOffset = new Vector3(-x, 0, 0);
+            }
+            else if (horizontal < 0 && direction != Vector3.right)
+            {
+                direction = Vector3.left;
+                transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z + 90);
+                BodyOffset = new Vector3(x, 0, 0);
+            }
+
+            if (vertical > 0 && direction != Vector3.down)
+            {
+                direction = Vector3.up;
+                transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z);
+                BodyOffset = new Vector3(0, -x, 0);
+            }
+            else if (vertical < 0 && direction != Vector3.up)
+            {
+                direction = Vector3.down;
+                transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z + 180);
+                BodyOffset = new Vector3(0, x, 0);
+            }
+
+            StartCoroutine(Snake_Move(direction));
+            for (int i = (segments.Count - 1); i > 0; i--)
+            {
+                segments[i].transform.SetPositionAndRotation(segments[i - 1].transform.localPosition + BodyOffset, segments[i - 1].transform.rotation);
+            }
+        }
+
     }
 
-    private void Movement(float Horizontal, float Vertical)
+    private IEnumerator Snake_Move(Vector3 Direction)
     {
+        float timePassed = 0f;
+        IsMoving = true;
+        orignalPos = transform.position;
+        TargetPos = orignalPos + Direction;
 
-        if (Horizontal > 0)
+        while(timePassed < TimetoMove)
         {
-            direction = Vector2Int.right;
-            transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z - 90f);
+            transform.position = Vector3.Lerp(orignalPos, TargetPos, (timePassed / TimetoMove));
+            timePassed += Time.deltaTime;
+            yield return null;
         }
-        else if (Horizontal < 0)
-        {
-            direction = Vector2Int.left;
-            transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z + 90);
-        }
-
-        if (Vertical > 0)
-        {
-            direction = Vector2Int.up;
-            transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z);
-        }
-        else if(Vertical < 0)
-        {
-            direction = Vector2Int.down;
-            transform.rotation = Quaternion.Euler(SpriteRotate.x, SpriteRotate.y, SpriteRotate.z + 180);
-        }
-        Vector2 MoveDistance = new Vector2(direction.x *Time.deltaTime * Speed, direction.y * Time.deltaTime * Speed);
-        transform.position = new Vector3(transform.position.x + MoveDistance.x, transform.position.y + MoveDistance.y, 0f);
-
-
+        transform.position = TargetPos;
+        IsMoving = false;
     }
 
     public void FoodEaten()
     {
-        Debug.Log("Snake ate the egg");
+        Grow();
+    }
+
+    private void Grow()
+    {
+        GameObject NewSegment = Instantiate(Segment);
+        NewSegment.transform.SetParent(Segment.transform.parent, false);
+        //NewSegment.transform.position = segments[segments.Count - 1].transform.position + BodyOffset;
+        segments.Add(NewSegment);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.tag == "Wall")
+        if (collision.tag == "Wall")
         {
-            this.enabled = false;
+            enabled = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Body")
+        {
+            enabled = false;
         }
     }
 }
